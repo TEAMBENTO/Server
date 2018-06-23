d0253b187510fbc6b4c32b07fcbf716ebc6f6121
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-22 19:10:44 -0700

    passing valid events model

--- test/unit/Event.test.js
+++ test/unit/Event.test.js
@@ -1,20 +1,22 @@
 const { assert } = require('chai');
+const { Types } = require('mongoose');
 const Event = require('../../lib/models/Event');
 
 describe('Event model', () => {
 
     it('vaild good model', () => {
         const data = {
-            name: '',
-            description: '',
-            type: '',
-            location: '',
+            name: 'PDX Monthly Run',
+            description: 'Come get your run on!',
+            type: ['running'],
+            location: 'Portland, OR',
             time: {
-                start: '',
-                end: ''
+                start: new Date(2018, 6, 22, 7, 30),
+                end: new Date(2018, 6, 22, 8, 30)
             },
-            host: [''],
-            attendance: ['']
+            host: [Types.ObjectId()],
+            group: [Types.ObjectId()],
+            attendance: [Types.ObjectId()]
         };
 
         const event = Event(data);

2d5eaa02dba539ef2f1ddefeda47629b9bf9c656
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-22 18:51:06 -0700

    failing vaild good model

--- test/unit/Event.test.js
+++ test/unit/Event.test.js
@@ -3,4 +3,23 @@ const Event = require('../../lib/models/Event');
 
 describe('Event model', () => {
 
+    it('vaild good model', () => {
+        const data = {
+            name: '',
+            description: '',
+            type: '',
+            location: '',
+            time: {
+                start: '',
+                end: ''
+            },
+            host: [''],
+            attendance: ['']
+        };
+
+        const event = Event(data);
+        data._id = event._id;
+        assert.deepEqual(event.toJSON(), data);
+        assert.isUndefined(event.validateSync());
+    });
 });

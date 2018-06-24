b040018cf8fed1f6f18dad1e8a17bb4e8f00a834
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-24 10:19:58 -0700

    events get test

--- test/e2e/events.test.js
+++ test/e2e/events.test.js
@@ -1,7 +1,7 @@
 const { assert } = require('chai');
 const request = require('./request');
 const { Types } = require('mongoose');
-const { dropCollection } = request('./db');
+const { dropCollection } = require('./db');
 
 describe('Event E2E API', () => {
 
@@ -11,6 +11,10 @@ describe('Event E2E API', () => {
         name: 'PDX Trail Run',
         description: 'A trail run in forest park.',
         type: 'running',
+        time: {
+            start: new Date('June 30, 2018 09:00:00'),
+            end: new Date('June 30, 2018 12:00:00')
+        },
         location: 'Forest Park',
         host: [Types.ObjectId()],
         group: [Types.ObjectId()],
@@ -21,8 +25,21 @@ describe('Event E2E API', () => {
         return request.post('/api/events')
             .send(race)
             .then(({ body }) => {
-                const { _id } = body;
+                const { _id, __v } = body;
                 assert.ok(_id);
+                assert.equal(__v, 0);
+                assert.deepEqual(body, {
+                    ...race,
+                    _id,
+                    __v
+                });
+            });
+    });
+
+    it('gets all albums', () => {
+        return request.get('/api/events')
+            .then(({ body }) => {
+                assert.deepEqual(body, [race]);
             });
     });
 

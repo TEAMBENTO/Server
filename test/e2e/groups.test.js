b7a183514ee2e00de535d2b1e07ad21d9f56a88a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-28 07:44:23 -0700

    removed only

--- test/e2e/groups.test.js
+++ test/e2e/groups.test.js
@@ -2,7 +2,7 @@ const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
 
-describe.only('groups e2e', () => {
+describe('groups e2e', () => {
 
     before(() => dropCollection('users'));
     before(() => dropCollection('profiles'));
